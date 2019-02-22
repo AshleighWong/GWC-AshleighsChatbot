e54fd36a353e550e77186fd50769347e759b668e
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2019-02-22 17:57:08 -0500

    Uploading Chatbot
    Girls who code python chatbot

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hey how was your day my brochochocho")
+answer = input("You:")
+print("Hey do u play Fn?")
+answer2 = input("You:")
+if(answer2 == "yes"):
+  print("Lets try playing then!" \
+  + "My username is namiuw." \
+  + "Whats yours?")
+elif(answer2 == "no"):
+  print("Oh okay, thats fine! You can try to play right now!"\
+  + "My usernane is namiuw." \
+  + "Whats yours")
+answer3 = input("You:")
+print("Im going to add " + answer3 + " ok?")
+answer4 = input("You:")
+
+for Phantop in range(0,6):
+  print("SeA PiCkLe??")
+  print("MC RN??")
+  print("Phantop fell from a high place")
+  
+info = ""
+while info != "sure":
+  info = input("Mc right now?")
+  print("play")
+  
+   
